a5aab8d46868ea16252f456d34339c65b7c53b5e
barahaeva <150849350+barahaeva@users.noreply.github.com>
2023-12-10 18:23:28 +0300

    in processing

---
+++ MovieQuiz/Services/QuestionFactoryDelegate.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryDelegate.swift
+//  MovieQuiz
+//
+//  Created by Karina ❦ on 10.12.2023.
+//
+
+import Foundation
---
+++ MovieQuiz/Services/QuestionFactoryProtocol.swift
@@ -0,0 +1,8 @@
+//
+//  QuestionFactoryProtocol.swift
+//  MovieQuiz
+//
+//  Created by Karina ❦ on 10.12.2023.
+//
+
+import Foundation
